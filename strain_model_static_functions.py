d7693e2d9062da5eff3c3aebe99c1bb6ea050a8c
bely12 <b.ely927@gmail.com>
2025-03-05 21:51:25 -0500

    added static strain sim

---
+++ strain_model_static_functions.py
@@ -0,0 +1,290 @@
+import numpy as np
+import random
+import itertools
+from itertools import combinations
+import math
+#import copy
+
+# reboot for static strains
+
+# general functions
+def hamming_distance(string1, string2):
+  distance = 0
+  for i in range(len(string1)):
+      if string1[i] != string2[i]:
+          distance += 1
+  return distance
+
+def assign_hs(spec_type): # assigns a single hs value
+  if spec_type == 'generalist':
+    hs_val = 0.5
+  if spec_type == 'rodent':
+    hs_val = random.uniform(0.51, 1.0)
+  if spec_type == 'bird':
+    hs_val = random.uniform(0.0, 0.49)
+  return round(hs_val, 2)
+
+def assign_hs_v2(spec_types): # takes a list of strain hs types; can be used to create their values within Pathogen class
+  hs_vals = []
+  for spec in spec_types:
+    if spec == 'generalist':
+      hs_val = 0.5
+    if spec == 'rodent':
+      hs_val = round(random.uniform(0.51, 1.0),2)
+    if spec == 'bird':
+      hs_val = round(random.uniform(0.0, 0.49),2)
+    hs_vals.append(hs_val)
+  return hs_vals
+
+def assign_antigen(n): # assigns antigens randomly to each strain being created; use when creating Pathogen class
+  antigen_vals = []
+  for i in range(n):
+    antigen_vals.append(round(random.uniform(0.0, 2.0),2))
+  return antigen_vals
+
+def antigen_distance(antigen1, antigen2):
+  lrg = max(antigen1, antigen2)
+  sml = min(antigen1, antigen2)
+  dist1= sml + (2.0-lrg)
+  dist2 = lrg - sml
+  return min(dist1, dist2)
+
+def dispersion(antigen_value_list): # this only works correctly for 3 strains!
+  all_dists = []
+  combos = list(itertools.combinations(antigen_value_list, 2))
+  for pair in combos:
+    all_dists.append(antigen_distance(pair[0], pair[1]))
+  return sum(all_dists)
+
+def transmission_probability(strain, host):
+  dists = []
+  for i in range(len(host['infections'])):
+    dists.append(antigen_distance(strain['antigen'], host['infections'][i]['antigen']))
+
+  if host['host_type'] == 'rodent':
+    return (strain['hs']**2) * (1 - math.exp(-2.0 * min(dists)))
+  
+  if host['host_type'] == 'bird':
+    return (1-(strain['hs']**2)) * (1 - math.exp(-2.0 * min(dists)))
+
+def tick2host_transmission(tick, host):
+  # create the transmission community from tick
+  if tick['strains'] == []:
+    return []
+  else:
+    tick_transmission_community = random.sample(tick['strains'], k = random.randint(1,len(tick['strains'])))
+
+  # transmission
+  transmitted_strains = []
+  if host['infections'] == []:
+    for strain in tick_transmission_community:
+      
+      if host['host_type'] == 'rodent':
+        transmission_prob = (strain['hs']**2) * (1 - math.exp(-2.0))
+      
+      if host['host_type'] == 'bird':
+        transmission_prob = (1-(strain['hs']**2)) * (1 - math.exp(-2.0))
+      
+      if random.random() < transmission_prob:
+        transmitted_strains.append(strain)
+    return transmitted_strains
+
+  else:
+    for strain in tick_transmission_community:
+      if random.random() < transmission_probability(strain, host):
+        transmitted_strains.append(strain)
+    return transmitted_strains
+
+
+def host2tick_transmission(host):
+  # create transmission community from host
+  if host['infections'] == []:
+    return []
+  else:
+    if random.random() < 0.7:
+      number_of_strains = random.randint(1, len(host['infections']))
+      host_transmission_community = random.sample(host['infections'], number_of_strains)
+    else:
+      return []
+
+  # transmission
+  transmitted_strains = []
+  for strain in host_transmission_community:
+    if random.random() < 0.7:
+      transmitted_strains.append(strain)
+  return transmitted_strains
+
+################################################################################
+# host population
+class Host:
+  def __init__(self, n_rodents, n_birds):
+
+    hosts = []
+    existing_ids = []
+    for i in range(n_rodents):
+      while True: # assign a unique id
+        new_id = random.randint(10000, 99999)
+        if new_id not in existing_ids:
+          id = new_id
+          break
+      existing_ids.append(id)
+      hosts.append({'id': id, 'infections': [], 'host_type': 'rodent', 'born': random.choice(range(1,76))})
+
+    for i in range(n_birds):
+      while True:
+        new_id = random.randint(10000, 99999)
+        if new_id not in existing_ids:
+          id = new_id
+          break
+      existing_ids.append(id)
+      hosts.append({'id': id, 'infections': [], 'host_type': 'bird', 'born': random.choice(range(1,76))})
+
+    # define pops and sizes
+    self.pop = hosts
+    self.pop_size = len(self.pop)
+    self.rodent_pop = [d for d in self.pop if d.get('host_type') == 'rodent']
+    self.rodent_pop_size = len(self.rodent_pop)
+    self.bird_pop = [d for d in self.pop if d.get('host_type') == 'bird']
+    self.bird_pop_size = len(self.bird_pop)
+
+  def refresh(self, day):
+    for host in self.pop:
+      if host['born'] == day:
+        host['infections'] = []
+
+################################################################################
+# pathogen population
+class Pathogen:
+  def __init__(self, n, hs, antigen): # hs and antigen args should be lists equal to length of n
+
+    self.strain_community = []
+    for i in range(n):
+      self.strain_community.append({'strain': str(i), 'hs': hs[i], 'antigen': antigen[i]})
+
+
+################################################################################
+# vector population
+class Vector:
+  def __init__(self, pop_size, strains, lam):
+
+    if pop_size % 2 != 0:
+      raise ValueError("The value of pop_size must be divisible by 2.")
+
+    # define the pathogens and starting infection status
+    nymph_infections = np.random.poisson(lam, size=(pop_size // 2)) # poisson distribution for tick infection status
+
+    # initialize the population of ticks
+    tick_pop = []
+    existing_ids = []
+
+    # create nymphs
+    for i in range(len(nymph_infections)):
+      while True:
+        new_id = random.randint(10000, 99999)
+        if new_id not in existing_ids:
+          id = new_id
+          break
+      existing_ids.append(id)
+      if nymph_infections[i] == 0:
+        tick_pop.append({'id': id, 'stage': 'nymph', 'strains': []})
+      else:
+        if nymph_infections[i] >len(strains):
+          variants = random.sample(strains, k=len(strains))
+          tick_pop.append({'id': id, 'stage': 'nymph', 'strains': variants})
+        if nymph_infections[i] <len(strains):
+          variants = random.sample(strains, k=nymph_infections[i])
+          tick_pop.append({'id': id, 'stage': 'nymph', 'strains': variants})
+
+    # create larva
+    for i in range(pop_size // 2):
+      while True:
+        new_id = random.randint(10000, 99999)
+        if new_id not in existing_ids:
+          id = new_id
+          break
+      existing_ids.append(id)
+      tick_pop.append({'id': id, 'stage': 'larva', 'strains': []})
+
+    # for use in defining attributes
+    nymph_pop = [d for d in tick_pop if d.get('stage') == 'nymph']
+
+    strain_pop = []
+    for d in nymph_pop:
+      if d['strains'] != []:
+        for i in range(len(d['strains'])):
+          strain_pop.append(d['strains'][i])
+
+    # define important stuff
+    self.pop = tick_pop
+    self.nymph_pop = nymph_pop
+    self.num_carried = sum(len(tick['strains']) for tick in self.nymph_pop) / len(self.nymph_pop)
+    self.pop_size = len(self.pop)
+    self.year = 0
+    self.poisson_infection_status = nymph_infections
+    self.infection_rate = round(sum(1 for d in nymph_pop if d.get("strains")) / len(nymph_pop) * 100, 3)
+
+
+  ## function molts larva to nymphs and hatches new nymphs; current nymphs age out to adults and are not included in sim
+  def update_pop(self):
+    # remove old nymphs
+    filtered_list = [d for d in self.pop if d.get('stage') != 'nymph']
+    updated_pop = filtered_list
+
+    # molt larva to nymphs
+    for tick in updated_pop:
+      if tick['stage'] == 'larva':
+        tick['stage'] = 'nymph'
+    existing_ids = []
+    for i in range(len(updated_pop)):
+      existing_ids.append(updated_pop[i]['id'])
+
+    # create new larva
+    for i in range(self.pop_size // 2):
+      while True:
+        new_id = random.randint(10000, 99999)
+        if new_id not in existing_ids:
+          id = new_id
+          break
+      existing_ids.append(id)
+      updated_pop.append({'id': id,'stage': 'larva', 'strains': []})
+
+    # redefine pop
+    self.pop = updated_pop
+
+    # for use in defining attributes
+    nymph_pop = [d for d in self.pop if d.get('stage') == 'nymph']
+    strain_pop = []
+    for d in nymph_pop:
+      if d['strains'] != []:
+        for i in range(len(d['strains'])):
+          strain_pop.append(d['strains'][i])
+
+    # redefine attributes
+    self.nymph_pop = nymph_pop # I probably don't need this but I'll leave it here for now
+    self.num_carried = sum(len(tick['strains']) for tick in self.nymph_pop) / len(self.nymph_pop)
+    self.infection_rate = round(sum(1 for d in nymph_pop if d.get("strains")) / len(nymph_pop) * 100, 3)
+    self.year += 1
+
+
+  # function assigns a host for each tick to bite and the day/s it bites on
+  def interaction(self, hosts):
+    interaction_list = []
+
+    # assign tick-host interactions
+    for i in range(len(self.pop)):
+      # larva bites
+      if self.pop[i]['stage'] == 'larva':
+        z = random.randint(1, len(hosts)-1)
+        chosen_host = hosts[z]['id']
+        bite_day = random.randint(75, 150)
+      # nymph bites
+      if self.pop[i]['stage'] == 'nymph':
+        z = random.randint(1, len(hosts)-1)
+        chosen_host = hosts[z]['id']
+        bite_day = random.randint(1, 75)
+
+      # add info to interaction list
+      interaction_list.append({'tick_id': self.pop[i]['id'],
+                              'host_id': chosen_host,
+                              'bite_day': bite_day})
+    return interaction_list
---
+++ strain_model_static_sim.py
@@ -0,0 +1,154 @@
+import strain_model_static_functions as bb
+import pandas as pd
+import argparse
+from argparse import RawTextHelpFormatter
+
+### ARGUMENTS ###
+parser = argparse.ArgumentParser(
+    description='Vector borne pathogen evolution simulation; takes defined sets of 2-3 strains as a community and tests stability'
+    ' \n', formatter_class=RawTextHelpFormatter)
+
+parser.add_argument('-n_strains', type=int, help='how many strains in the community; 2 or 3')
+
+def list_of_strings(arg):
+    return arg.split(',')
+parser.add_argument('-spec_types', type=list_of_strings)
+
+parser.add_argument('-vector_pop_size', type=int, help='size of vector(tick) population; needs to be at least 50 and even number')
+parser.add_argument('-rodent_pop_size', type=int, help='number of rodents in host pop')
+parser.add_argument('-bird_pop_size', type=int, help='number of birds in host pop')
+
+parser.add_argument('-years', type=int, help='number of years to simulate')
+parser.add_argument('-replicates', type=int, default=1, help='number of times to run sim with specified conditions')
+parser.add_argument('-out', default='results', help='prefix for output file')
+
+args = parser.parse_args()
+
+
+
+### define parameters ###
+n_strains = args.n_strains
+spec_types = args.spec_types
+vector_pop_size = args.vector_pop_size
+rodent_pop_size = args.rodent_pop_size
+bird_pop_size = args.bird_pop_size
+sim_years = args.years
+repeat = args.replicates
+
+run_tag = 0
+all_data = []
+
+### start new replication ###
+for i in range(repeat):
+    run_tag = run_tag + 1
+
+### initialize pathogen, vector, host populations ###
+    patho = bb.Pathogen(n = n_strains,
+                hs = bb.assign_hs_v2(spec_types),
+                antigen = bb.assign_antigen(n_strains))
+
+    ticks = bb.Vector(pop_size=vector_pop_size,
+                    strains=patho.strain_community,
+                    lam=1.0)
+
+    hosts = bb.Host(n_rodents= rodent_pop_size,
+                n_birds= bird_pop_size)
+
+
+    ### data collection ###
+    sim_hs_vals = []
+    sim_antigen_vals = []
+    for i in range(len(patho.strain_community)):
+        sim_hs_vals.append(patho.strain_community[i]['hs'])
+        sim_antigen_vals.append(patho.strain_community[i]['antigen'])
+
+    if n_strains == 2:
+        antigen_variation = round(bb.antigen_distance(sim_antigen_vals[0], sim_antigen_vals[1]),2)
+    if n_strains == 3:
+        antigen_variation = round(bb.dispersion(sim_antigen_vals),2)
+
+    run_data = {'run_tag': run_tag,
+            'v_pop': vector_pop_size,
+            'rodent_pop': rodent_pop_size,
+            'bird_pop': bird_pop_size,
+            'hs_vals': sim_hs_vals,
+            'antigen_vals': sim_antigen_vals,
+            'antigen_variation': antigen_variation}
+
+
+    ### start sim ###
+    for year in range(sim_years):
+        # set tick/host interactions for the year
+        interactions = ticks.interaction(hosts.pop)
+
+        # sim through the days of the year
+        for day in range(1, 151):
+
+            # refresh the host pop for births and deaths
+            hosts.refresh(day)
+
+            # iterate through interactions to see if there is a bite for current day
+            for j in range(len(interactions)):
+                if interactions[j]['bite_day'] == day:
+
+                    # find the host and tick involved in interaction
+                    current_host = next((item for item in hosts.pop if item["id"] == interactions[j]['host_id']), None)
+                    current_tick = next((item for item in ticks.pop if item["id"] == interactions[j]['tick_id']), None)
+
+                    # determine strains to be transmitted from tick to host
+                    strains_from_tick = bb.tick2host_transmission(tick=current_tick, host=current_host)
+
+                    # determine strains to be transmitted from host to tick
+                    strains_from_host = bb.host2tick_transmission(current_host)
+
+                    # update host infection community
+                    for item in hosts.pop:
+                        if item["id"] == current_host['id']:
+                            item["infections"] = current_host['infections'] + [item for item in strains_from_tick if (item not in current_host['infections'])]
+                            break
+
+                    # update tick infection community
+                    for item in ticks.pop:
+                        if item['id'] == current_tick['id']:
+                            item['strains'] = current_tick['strains'] + [item for item in strains_from_host if item not in current_tick['strains']]
+                            break
+
+        # update populations
+        ticks.update_pop()
+
+
+    # identify what strains are left after sim finishes
+    seen = []
+    for i in range(len(ticks.nymph_pop)):
+        if ticks.nymph_pop[i]['strains'] != []:
+            for j in range(len(ticks.nymph_pop[i]['strains'])):
+                if ticks.nymph_pop[i]['strains'][j] not in seen:
+                    seen.append(ticks.nymph_pop[i]['strains'][j])
+
+    # identify the trait values of remaining strains
+    if len(seen) != 0:
+        final_sim_hs_vals = []
+        final_sim_antigen_vals = []
+        for i in range(len(seen)):
+            final_sim_hs_vals.append(seen[i]['hs'])
+            final_sim_antigen_vals.append(seen[i]['antigen'])
+    else:
+        final_sim_antigen_vals = 'none'
+        final_sim_hs_vals = 'none'
+
+    # label sim outcome as stable or unstable
+    if len(seen) == len(patho.strain_community):
+        result = "stable"
+    else:
+        result = "unstable"
+
+    # add results data to dictionary
+    run_data.update({'final_hs': final_sim_hs_vals,
+                    'final_ant': final_sim_antigen_vals,
+                    'result': result})
+
+    # add to all data for batch run collection
+    all_data.append(run_data)
+
+df = pd.DataFrame(all_data)
+df.to_csv(args.out+'_static_sim_results.tsv', mode= 'w', sep='\t', index=False, header=True)
